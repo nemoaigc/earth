053cfa0e3b53af66085cb8376d4861f04c3db975
Eris <panajanemo@gmail.com>
2026-04-13 20:10:33 +0800

    Reduce mesh to 300x300 (400 was too heavy)

--- src/globe/terrain.ts
+++ src/globe/terrain.ts
@@ -60,7 +60,7 @@ const COLOR_OCEAN_DEEP = new THREE.Color('#22aadd');
 const COLOR_OCEAN_SHALLOW = new THREE.Color('#55ccee');
 
 export function generateTerrain(): TerrainData {
-  const geometry = new THREE.SphereGeometry(GLOBE_RADIUS, 400, 400);
+  const geometry = new THREE.SphereGeometry(GLOBE_RADIUS, 300, 300);
   const posAttr = geometry.getAttribute('position');
   const vertexCount = posAttr.count;
 
